3bac840e587cdfc9493d28cded663fce8b762dd2
kuihua <michellelin2235@gmail.com>
2024-10-10 13:31:47 -0700

    added basic sound system

---
+++ Assets/Scripts/SwitchMusicTrigger.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMusicTrigger : MonoBehaviour
+{
+
+    public AudioClip newTrack;
+    private AudioManager audioManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioManager = FindObjectOfType<AudioManager>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.layer == LayerMask.NameToLayer("Player Layer"))
+        {
+            if(newTrack != null)
+            {
+            audioManager.ChangeBGM(newTrack);
+            }
+        }
+    }
+
+}
