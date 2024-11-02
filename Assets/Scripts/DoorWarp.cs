bd24b9170f2315edb96f370a639571729916fba2
Jyl7hf <lijillian@hotmail.com>
2024-11-01 21:02:12 -0700

    edited doorwarp and added icons

--- Assets/Scripts/DoorWarp.cs
+++ Assets/Scripts/DoorWarp.cs
@@ -6,6 +6,7 @@ public class DoorWarp : MonoBehaviour
 {
     public GameObject Player;
     public Vector2 NewLocation;
+    public Transform target;
     public ScreenFader Sf;
 
     private SpriteRenderer sr;
@@ -16,30 +17,36 @@ public class DoorWarp : MonoBehaviour
     {
         Sf = GameObject.Find("Canvas/Screen Fader").GetComponent<ScreenFader>();
         sr = GetComponent<SpriteRenderer>();
+
+        Player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.E) && playerDetected && Player != null) {
+        if(Input.GetKeyDown(KeyCode.E) && playerDetected //&& Player != null
+        && Player.GetComponent<PlayerMovement>().enabled) {
             StartCoroutine(warp());
         }
     }
 
     IEnumerator warp() {
         Debug.Log("start");
+        Player.GetComponent<PlayerMovement>().Stop();
+        Player.GetComponent<PlayerMovement>().enabled = false;
         yield return StartCoroutine(Sf.FadeToBlack());
-        // Controller.GetComponent<PlayerControl>().getCurrentPlayer().position = NewLocation;
-        Player.transform.position = NewLocation;
+        // Player.transform.position = NewLocation;
+        Player.transform.position = target.position;
         yield return new WaitForSeconds(0.5f);
         yield return StartCoroutine(Sf.FadeToClear());
+        Player.GetComponent<PlayerMovement>().enabled = true;
         Debug.Log("end");
     }
 
     void OnTriggerEnter2D(Collider2D collider) {
         if(collider.CompareTag("Player")) {
             playerDetected = true;
-            Player = collider.gameObject;
+            // Player = collider.gameObject;
             sr.enabled = true;
         }
     }
@@ -48,7 +55,7 @@ public class DoorWarp : MonoBehaviour
         if(collider.CompareTag("Player")) {
             playerDetected = false;
             sr.enabled = false;
-            Player = null;
+            // Player = null;
         }
     }
 }
