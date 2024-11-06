ef0761f45abad6d7a025cee09748ef5ad35ef2c0
Jyl7hf <lijillian@hotmail.com>
2024-11-06 10:41:23 -0800

    added Coleson file

--- Assets/Scripts/CheckPassword.cs
+++ Assets/Scripts/CheckPassword.cs
@@ -8,11 +8,15 @@ public class CheckPassword : MonoBehaviour, IPointerClickHandler
 {
     public TMP_InputField inputField;
 
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
+    public GameObject loginScreen;
+    public GameObject fileScreen;
+    public GameObject closeButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        loginScreen = transform.parent.gameObject;
+    }
 
     // // Update is called once per frame
     // void Update()
@@ -25,6 +29,10 @@ public class CheckPassword : MonoBehaviour, IPointerClickHandler
         // popupWindow.SetActive(false);
         if(inputField.text == "Nicole") {
             Debug.Log("Logging in");
+            closeButton.GetComponent<ClosePopup>().enabled = false;
+            closeButton.GetComponent<FinishPuzzle>().enabled = true;
+            fileScreen.SetActive(true);
+            loginScreen.SetActive(false);
         }
         else {
             Debug.Log("Wrong password");
