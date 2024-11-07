107d688d7cfa684881438dc8c4f9859d3e327a64
Jyl7hf <lijillian@hotmail.com>
2024-11-06 23:24:10 -0800

    fire alarm pull

---
+++ Assets/Scripts/FireLever.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class FireLever : MonoBehaviour, IDragHandler
+{
+    public Transform start, end;
+    public OpenPopup popupInteract;
+    GameObject popupWindow;
+    GameObject Player;
+
+    bool pulled;
+    float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        popupWindow = transform.parent.gameObject;
+        Player = GameObject.Find("Player");
+        pulled = false;
+        timer = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pulled) {
+            timer -= Time.deltaTime;
+            if(timer <= 0) {
+                Player.GetComponent<PlayerMovement>().enabled = true;
+                popupInteract.gameObject.SetActive(false);
+                popupWindow.SetActive(false);
+            }
+        }
+        else if(transform.position.y <= end.position.y) {
+            pulled = true;
+            // print("fire alarm pulled");
+        }
+    }
+
+    public void OnDrag(PointerEventData eventData) {
+        if(!pulled) {
+            float yPos;
+            if(Input.mousePosition.y > start.position.y) {
+                yPos = start.position.y;
+            }
+            else if(Input.mousePosition.y < end.position.y) {
+                yPos = end.position.y;
+            }
+            else {
+                yPos = Input.mousePosition.y;
+            }
+            transform.position = new Vector3(transform.position.x, yPos, transform.position.z);
+        }
+    }
+}
