48c20b23e64e5dce66c61c655af604a6da5e2325
kuihua <michellelin2235@gmail.com>
2024-11-08 13:00:01 -0800

    activate cutscene on key press

--- Assets/Scripts/ActivateCutscene.cs
+++ Assets/Scripts/ActivateCutscene.cs
@@ -5,12 +5,34 @@ public class ActivateCutscene : MonoBehaviour
 {
     [SerializeField] private PlayableDirector playableDirector;
 
-    // activate the cutscene if the player enters the trigger area
-    // disables the trigger afterwards so it won't happen again
-    private void OnTriggerEnter2D (Collider2D collision) {
-        if (collision.CompareTag("Player")){
-            playableDirector.Play();
-            GetComponent<BoxCollider2D>().enabled = false;
+    [Header("Activate Cutscene With E. Disables collider after.")]
+    public bool isPressKeyToActivate;
+    private bool playerDetected;
+    public GameObject Player;
+    public bool hasPlayed; // for testing
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && playerDetected && Player != null) {
+            ActivateCutsceneKey();
         }
     }
+
+// activate the cutscene if the player enters the trigger area
+    // disables the trigger afterwards so it won't happen again
+    private void OnTriggerEnter2D (Collider2D collision) {
+        if (collision.CompareTag("Player") && !isPressKeyToActivate){
+            playableDirector.Play();
+            GetComponent<BoxCollider2D>().enabled = false;
+        } else {
+            playerDetected = true;
+        }
+    }
+
+    private void ActivateCutsceneKey () {
+        // function to play cutscene, disables this script when its done
+        playableDirector.Play();
+        GetComponent<ActivateCutscene>().enabled = false;
+    }
+
 }
