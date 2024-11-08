f1d7b9be9651ed3515f3f9e802a11475d2596547
kuihua <michellelin2235@gmail.com>
2024-11-08 13:03:18 -0800

    comments for activate cutscene script

--- Assets/Scripts/ActivateCutscene.cs
+++ Assets/Scripts/ActivateCutscene.cs
@@ -3,9 +3,9 @@ using UnityEngine.Playables;
 
 public class ActivateCutscene : MonoBehaviour
 {
-    [SerializeField] private PlayableDirector playableDirector;
+    [SerializeField] private PlayableDirector playableDirector; // timeline to play
 
-    [Header("Activate Cutscene With E. Disables collider after.")]
+    [Header("Activate Cutscene With E. Disables after.")]
     public bool isPressKeyToActivate;
     private bool playerDetected;
     public GameObject Player;
@@ -18,7 +18,7 @@ public class ActivateCutscene : MonoBehaviour
         }
     }
 
-// activate the cutscene if the player enters the trigger area
+    // activate the cutscene if the player enters the trigger area
     // disables the trigger afterwards so it won't happen again
     private void OnTriggerEnter2D (Collider2D collision) {
         if (collision.CompareTag("Player") && !isPressKeyToActivate){
