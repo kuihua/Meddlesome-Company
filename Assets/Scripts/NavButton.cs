dcffcdabfa7c100fe3e389b91533d2966c31aa40
Jyl7hf <lijillian@hotmail.com>
2024-11-08 23:50:25 -0800

    started Theo's system menu draft

---
+++ Assets/Scripts/NavButton.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class NavButton : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject otherScreen;
+    GameObject screen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screen = transform.parent.gameObject;
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        otherScreen.SetActive(true);
+        screen.SetActive(false);
+    }
+}
