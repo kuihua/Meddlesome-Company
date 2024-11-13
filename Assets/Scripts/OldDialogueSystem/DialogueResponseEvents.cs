f9ef0c92625c990d16381afbc3bc941badc90ea7
kuihua <michellelin2235@gmail.com>
2024-11-12 22:04:53 -0800

    commented out old dialogue system

--- Assets/Scripts/OldDialogueSystem/DialogueResponseEvents.cs
+++ Assets/Scripts/OldDialogueSystem/DialogueResponseEvents.cs
@@ -1,40 +1,40 @@
-using UnityEngine;
-using System;
+// using UnityEngine;
+// using System;
 
-public class DialogueResponseEvents : MonoBehaviour
-{
-    [SerializeField] private DialogueObject dialogueObject;
-    [SerializeField] private ResponseEvent[] events;
+// public class DialogueResponseEvents : MonoBehaviour
+// {
+//     [SerializeField] private DialogueObject dialogueObject;
+//     [SerializeField] private ResponseEvent[] events;
 
-    public DialogueObject DialogueObject => dialogueObject;
+//     public DialogueObject DialogueObject => dialogueObject;
 
-    public ResponseEvent[] Events => events;
+//     public ResponseEvent[] Events => events;
 
-    public void OnValidate() {
-        if (dialogueObject == null) return;
-        if (dialogueObject.Responses == null) return;
-        if (events != null && events.Length == dialogueObject.Responses.Length) return;
+//     public void OnValidate() {
+//         if (dialogueObject == null) return;
+//         if (dialogueObject.Responses == null) return;
+//         if (events != null && events.Length == dialogueObject.Responses.Length) return;
 
-        if (events == null) {
-            // [] means evaluated at build time
-            events = new ResponseEvent[dialogueObject.Responses.Length];
-        } else {
-            Array.Resize(ref events, dialogueObject.Responses.Length);
-        }
+//         if (events == null) {
+//             // [] means evaluated at build time
+//             events = new ResponseEvent[dialogueObject.Responses.Length];
+//         } else {
+//             Array.Resize(ref events, dialogueObject.Responses.Length);
+//         }
 
-        for (int i = 0; i < dialogueObject.Responses.Length; i++) {
-            Response response = dialogueObject.Responses[i];
+//         for (int i = 0; i < dialogueObject.Responses.Length; i++) {
+//             Response response = dialogueObject.Responses[i];
 
-            if (events[i] != null) {
-                // the name is set in the inspector so you can see which events you want to happen under which response
-                events[i].name = response.ResponseText;
-                continue;
-            }
+//             if (events[i] != null) {
+//                 // the name is set in the inspector so you can see which events you want to happen under which response
+//                 events[i].name = response.ResponseText;
+//                 continue;
+//             }
 
-            // giving the event a name
-            events[i] = new ResponseEvent() {
-                name = response.ResponseText
-            };
-        }
-    }
-}
+//             // giving the event a name
+//             events[i] = new ResponseEvent() {
+//                 name = response.ResponseText
+//             };
+//         }
+//     }
+// }
