452b1e86ea448bf0b6820394e12e549c5c7e1f94
Jyl7hf <lijillian@hotmail.com>
2024-11-20 21:16:38 -0800

    started employee db stuff

--- Assets/Scripts/StartScreen.cs
+++ Assets/Scripts/StartScreen.cs
@@ -1,15 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+// using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+// using UnityEngine.UI;
 
 public class StartScreen : MonoBehaviour/*, IPointerClickHandler*/
 {
     ScreenFader Sf;
     bool enteringScene;
-
     // public Button quitButton;
 
     // Start is called before the first frame update
