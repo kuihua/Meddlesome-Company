a92b3a7071bf5280ce3e244f00d07b8c3269692b
Jyl7hf <lijillian@hotmail.com>
2024-11-22 17:20:48 -0800

    meeting schedule and cut wires

---
+++ Assets/Scripts/WireCut.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WireCut : MonoBehaviour
+{
+    public GameObject[] wireOrder;
+    int step = 0;
+
+    public GameObject failScreen;
+    ScreenFader Sf;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sf = GameObject.Find("Canvas/Screen Fader").GetComponent<ScreenFader>();
+    }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+
+    public void cutWire(GameObject wire) {
+        if(wire == wireOrder[step]) {
+            step += 1;
+            if(step == wireOrder.Length) {
+                Debug.Log("wires cut correctly");
+            }
+        }
+        else {
+            Debug.Log("game over");
+            StartCoroutine(fail());
+        }
+    }
+
+    IEnumerator fail() {
+        yield return StartCoroutine(Sf.FadeToBlack());
+        failScreen.SetActive(true);
+        gameObject.SetActive(false);
+    }
+}
