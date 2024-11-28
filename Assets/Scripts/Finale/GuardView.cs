b0cc1a520a4dbba0ccf0c03c31aca2fb522f07b9
Jyl7hf <lijillian@hotmail.com>
2024-11-28 11:46:26 -0800

    started splitting scenes

--- Assets/Scripts/Finale/GuardView.cs
+++ Assets/Scripts/Finale/GuardView.cs
@@ -4,21 +4,31 @@ using UnityEngine;
 
 public class GuardView : MonoBehaviour
 {
+    public GameObject CaughtScreen;
+    ScreenFader Sf;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // CaughtScreen = GameObject.Find("Canvas/Caught screen");
+        Sf = GameObject.Find("Canvas/Screen Fader").GetComponent<ScreenFader>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    // // Update is called once per frame
+    // void Update()
+    // {
         
-    }
+    // }
 
-    void OnTriggerEnter2D(Collider2D collider) {
+    IEnumerator OnTriggerEnter2D(Collider2D collider) {
         if(collider.CompareTag("Player")) {
             Debug.Log("seen");
+            yield return StartCoroutine(Caught());
         }
+    }
+
+    IEnumerator Caught() {
+        yield return StartCoroutine(Sf.FadeToBlack());
+        CaughtScreen.SetActive(true);
     }
 }
