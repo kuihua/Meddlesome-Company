c97c0d317198af9c8a106905b75f84fb571421ca
Jyl7hf <lijillian@hotmail.com>
2024-11-30 01:12:42 -0800

    A1 tutorial keybinds

---
+++ Assets/Scripts/KeybindMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeybindMove : MonoBehaviour
+{
+    float timer = 5;
+    bool pressed = false;
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.Space)) {
+            pressed = true;
+        }
+    }
+
+    void FixedUpdate() {
+        if(timer > 0 && pressed) {
+            timer -= Time.fixedDeltaTime;
+            if(timer <= 0) {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
